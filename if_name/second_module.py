f7d72e052887a9899aa5705115e25fc5db1bb033
Daroc513 <rakeemembrose@gmail.com>
2024-01-04 22:42:24 +0000

    Playing with python pip packages

---
+++ if_name/second_module.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# The name of the import matches the file name (minus the .py)
+# These two files MUST BE IN THE SAME DIRECTORY
+
+import first_module
+
+first_module.main() # ADD THIS LINE
+
+print("Module #2 Name=", __name__)
