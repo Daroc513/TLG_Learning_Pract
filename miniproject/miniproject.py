7661da0a24fada8cfa1469c5dcf6ee7c047fcfae
Daroc513 <rakeemembrose@gmail.com>
2024-01-04 23:30:39 +0000

    TLG mini project. Grade calculator

---
+++ miniproject/miniproject.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+grade = float(input("Enter your grade:"))
+
+if grade >= 90:
+    print("A, you're excellent job!")
+
+elif grade >= 80:
+    print("B, not bad you're smarter than average!")
+
+elif grade >= 70:
+    print("C, as Tony from the Sopranos would say 'You're average at best!'")
+
+elif grade >= 60:
+    print("D, jesus do better. God speed my friend!")
+
+elif grade <= 59:
+    print("F, get the f out")
